52551f35c1aff02a788d0d08ef19c4a3b49d2455
Rob Ballantyne <raballantyne@gmail.com>
2019-06-28 17:21:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,16 @@ def using_push(array, string)
   array.push(string)
 end
 
+def using_unsift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def ppo_with_args(array)
+  array.pop(2)
+end
+
+def
