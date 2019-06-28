edea4b7666acbf1dd7a9f8f1559c137d7e34fbaa
Rob Ballantyne <raballantyne@gmail.com>
2019-06-28 17:24:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,4 +26,19 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_uniq(a)
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete_at(array, index)
+  array.delete(index)
+end
+
+
+  
+  
+  
